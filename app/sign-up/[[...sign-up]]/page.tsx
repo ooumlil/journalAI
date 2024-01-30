e71cc1f06f5148b27d0e2e7f21589f7dcc9799ca
Oussama Oumlil <oussamaoumlil1@gmail.com>
2024-01-30 03:05:07 +0100

    mostly done with authentication using clerk

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <div className="h-full flex items-center justify-center">
-      <SignUp />
+      <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
     </div>
   );
 }
