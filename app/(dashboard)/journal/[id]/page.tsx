7d2365dbc12055d94212eba4e8a642d5139eca3f
Oussama Oumlil <oussamaoumlil1@gmail.com>
2024-02-16 02:35:32 +0100

    moved the analysis data to the editor client component

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -22,36 +22,9 @@ export default async function EntryPage({
   params: { id: string };
 }) {
   const entry = await getEntry(params.id);
-  const { summary, subject, mood, negative, color }: any = entry?.analysis;
-  const analysisData = [
-    { name: 'Summary', value: summary },
-    { name: 'Subject', value: subject },
-    { name: 'Mood', value: mood },
-    { name: 'Negative', value: `${negative}` },
-  ];
   return (
-    <div className="h-full w-full grid grid-cols-3">
-      <div className="col-span-2">
+    <div className="h-full w-full">
         <Editor entry={entry} />
-      </div>
-      <div className="border-l border-black/10">
-        <div className="p-8" style={{ backgroundColor: color }}>
-          <h2 className="text-2xl">Ai stuff</h2>
-        </div>
-        <div>
-          <ul>
-            {analysisData.map((item) => (
-              <li
-                key={item.name}
-                className="flex items-center justify-between border-t border-b border-black/10 p-2"
-              >
-                <span className="font-semibold text-lg">{item.name}</span>
-                <span>{item.value}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
--- components/editor.tsx
+++ components/editor.tsx
@@ -7,23 +7,54 @@ import { useAutosave } from 'react-autosave';
 export default function Editor({ entry }: { entry: any }) {
   const [value, setValue] = useState(entry.content);
   const [isLoading, setIsLoading] = useState(false);
+  const [analysis, setAnalysis] = useState(entry.analysis);
 
   useAutosave({
     data: value,
     onSave: async (_value) => {
       setIsLoading(true);
-      const updatedEntry = await updateEntry(entry.id, _value);
+      const data = await updateEntry(entry.id, _value);
+      setAnalysis(data?.analysis);
       setIsLoading(false);
     },
   });
+
+  const { summary, subject, mood, negative, color }: any = analysis;
+  const analysisData = [
+    { name: 'Summary', value: summary },
+    { name: 'Subject', value: subject },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: `${negative}` },
+  ];
   return (
-    <div className="w-full h-full">
-      {isLoading && <div>loading...</div>}
-      <textarea
-        className="w-full h-full p-8 text-xl outline-none"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+    <div className="w-full h-full  grid grid-cols-3">
+      <div className="col-span-2">
+        {isLoading && <div>loading...</div>}
+        <textarea
+          className="w-full h-full p-8 text-xl outline-none"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+
+      <div className="border-l border-black/10">
+        <div className="p-8" style={{ backgroundColor: color }}>
+          <h2 className="text-2xl">Ai stuff</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="flex items-center justify-between border-t border-b border-black/10 p-2"
+              >
+                <span className="font-semibold text-lg">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
