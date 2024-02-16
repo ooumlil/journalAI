4854793c50028e968e573ec35594873e4537aefd
Oussama Oumlil <oussamaoumlil1@gmail.com>
2024-02-16 02:34:06 +0100

    added links in the sidebar of the journal page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,25 @@
 import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 import { ReactNode } from 'react';
+
+const links = [
+  { href: '/', name: 'Home' },
+  { href: '/journal', name: 'Journal' },
+];
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="h-screen w-screen flex">
-      <aside className="w-1/4 border-r">aside</aside>
+      <aside className="w-1/4 border-r">
+        <div>Mood</div>
+        <ul>
+          {links.map((link) => (
+            <li className='p-4 text-xl' key={link.name}>
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
       <div className="w-3/4">
         <header className="h-[10%] border-b">
           <div className="h-full w-full flex items-center justify-end px-6">
